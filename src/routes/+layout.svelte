3d0485369b348640b068518d17680a72ee28b8f0
Guandor <emekdenizalgin@gmail.com>
2024-10-03 21:01:42 +0200

    change date formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,9 @@
 	<div class="bg-white shadow-lg rounded-lg p-6 border border-gray-200 mt-8">
 		<p>
 			Made by <a href="https://denizalgin.com/">Deniz</a> 🧙‍♂️ - Open source at
-			<a href="https://github.com/Guandor/gads-resource-finder">Github</a> 💻 - Last update at {new Date().toLocaleDateString()}
+			<a href="https://github.com/Guandor/gads-resource-finder">Github</a> 💻 - Last update at {new Date().toLocaleDateString(
+				'en-GB'
+			)}
 		</p>
 	</div>
 </main>
